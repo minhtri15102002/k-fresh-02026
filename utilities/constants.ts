006de574eb781bd15e0f999479621420c2e2efcd
minhtri15102002 <minhtri15102002@gmail.com>
2026-04-23 11:22:44 +0700

    add feature

--- utilities/constants.ts
+++ utilities/constants.ts
@@ -1,10 +1,19 @@
 export class Constants {
-  static readonly ENV = process.env.ENV || 'qa';
-  static readonly BASE_URL = process.env.BASE_URL || 'https://ecommerce-playground.lambdatest.io/';
+  static readonly ENV = process.env.ENV || "qa";
+  static readonly BASE_URL =
+    process.env.BASE_URL || "https://ecommerce-playground.lambdatest.io/";
   static readonly LOGIN_URL = `${Constants.BASE_URL}/login`;
   static readonly SECURE_URL = `${Constants.BASE_URL}/secure`;
-  static readonly SUCCESS_MESSAGE = 'You logged into a secure area!';
-  static readonly LOGIN_USERNAME = process.env.LOGIN_USERNAME || 'tomsmith';
-  static readonly LOGIN_PASSWORD = process.env.LOGIN_PASSWORD || '';
-  static readonly USERS_JSON_FILE = './data/users.json';
+  static readonly SUCCESS_MESSAGE = "You logged into a secure area!";
+  static readonly LOGIN_USERNAME = process.env.LOGIN_USERNAME || "tomsmith";
+  static readonly LOGIN_PASSWORD = process.env.LOGIN_PASSWORD || "";
+  static readonly USERS_JSON_FILE = "./data/users.json";
+  static readonly PRODUCT_NAME = "HTC Touch HD";
+
+  static readonly TIMOUT = 5000; // Default timeout for waiting for elements
+  static readonly LOAD_STATE = {
+    NETWORK_IDLE: "networkidle",
+    DOM_CONTENT_LOADED: "domcontentloaded",
+    LOAD: "load",
+  } as const;
 }
